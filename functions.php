6202d113b5d562a4566a0114695e6e52ec9f3a61
Wesley Luk <wesleyluk@wesleyluk.com>
2013-04-20 16:08:26 -0700

    Lots of changes

--- functions.php
+++ functions.php
@@ -1,10 +1,29 @@
 <?php
 
-function my_scripts_method() {
+require_once(get_stylesheet_directory() . "/riichi.php");
+
+function mj_format_percent($num){
+	return sprintf("%d%%", $num * 100);
+}
+
+function mj_enqueue_scripts() {
     wp_enqueue_script( 'jQuery', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', null, '1.9.1', true );
     wp_enqueue_script( 'bootstrap',  get_stylesheet_directory_uri() . '/js/bootstrap.min.js', array('jQuery'), null, true );
 }
 
-add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+add_action( 'wp_enqueue_scripts', 'mj_enqueue_scripts' );
 
+
+function mj_wp_head(){
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />';
+}
+
+function mj_format_class($class){
+	if($class){
+		return "class=\"$class\"";
+	}
+	return "";
+}
+
+add_action( 'wp_head', 'mj_wp_head');
 ?>
--- hand-calculator.php
+++ hand-calculator.php
@@ -3,7 +3,14 @@
 Template Name: Hand Calculator
 */
 ?>
-
+<?php
+wp_enqueue_script( 'riichi-library',  get_stylesheet_directory_uri() . '/js/riichi-library/RiichiLibrary.js', null, true );
+wp_enqueue_script( 'hand-calculator',  get_stylesheet_directory_uri() . '/js/HandCalculator.js', array('riichi-library', 'jQuery'), null, true );
+wp_enqueue_style( 'hand-calculator-style', get_stylesheet_directory_uri() . '/css/HandCalculator.css' );
+?>
 <?php get_header(); ?>
-
+<script type="text/javascript">
+	var templateDir = "<?php echo get_stylesheet_directory_uri() ?>";
+</script>
+<div class="hand-calculator"></div>
 <?php get_footer(); ?>
